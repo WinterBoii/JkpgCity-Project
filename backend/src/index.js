140042212e3f323697657df08dd80d309523585b
Akida27 <Shukmah01a@gmail.com>
2024-02-01 16:22:05 +0100

    created database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,35 @@
 // index.js
+require('dotenv').config()
 const express = require('express');
+import storeJson from './stores.json';
 const app = express();
+const db = require('./db');
 const port = 3001;
 
 // Parse JSON bodies
 app.use(express.json());
 
+app.get('/setup', async (req, res) => {
+  try {
+    await db.setup(storeJson);
+    res.status(200).send('Setup complete');
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+app.get('/', async (req, res) => {
+  try {
+    const stores = await db.getAllStores();
+    res.status(200).send(stores);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
 const server = async () => {
-  await app.listen(port, () => {
+  await db.init();
+  app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
 }
