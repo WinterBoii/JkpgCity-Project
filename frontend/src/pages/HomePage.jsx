8d60e0129264c1341ec456243282326e3e5239c5
WinterBoii <wintercodess@gmail.com>
2024-02-22 16:17:37 +0100

    create title + font

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,38 @@
-import { Container } from '@mui/material';
+import { Box, Container, CssBaseline, Typography } from '@mui/material';
 
 function HomePage() {
 	return (
-		<Container maxWidth='xl'>
-			<h1>TODO Home</h1>
+		<Container
+			maxWidth='xl'
+			sx={{ marginTop: '5rem', color: 'third.text' }}
+		>
+			<Box sx={{ width: '100%' }}>
+				<Typography
+					variant='h2'
+					component={'h1'}
+					fontWeight={500}
+					mb={10}
+				>
+					Välkommen Till Ditt Jönköping
+				</Typography>
+				<Typography
+					variant='h4'
+					component={'h4'}
+					fontWeight={400}
+				>
+					Jönköping City är fyllt av butiker, kulter, upplevelser, caféer och{' '}
+					<br />
+					restauranger som ramas in av fantastiska grönområden och vackra sjöar.
+				</Typography>
+			</Box>
+			<Box
+				bgcolor={'third.bg'}
+				my={10}
+				py={2}
+				sx={{ borderRadius: '10px', width: '100%' }}
+			>
+				<CssBaseline>h1</CssBaseline>
+			</Box>
 		</Container>
 	);
 }
