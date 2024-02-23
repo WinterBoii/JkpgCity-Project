311e2e15352851bc0315b5284226da9948378c0a
WinterBoii <wintercodess@gmail.com>
2024-02-23 18:22:24 +0100

    tesst

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './lib/AuthProvider.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
   </BrowserRouter>
 );
