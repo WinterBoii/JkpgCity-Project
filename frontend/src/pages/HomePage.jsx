0e6ee2c287e21438b5bd78880b55aa1d08a50ffb
WinterBoii <wintercodess@gmail.com>
2024-02-28 11:31:46 +0100

    implement store page basics

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, Grid, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import shop1 from '../assets/shop1.jpg';
 import shop2 from '../assets/shop2.jpg';
+import TitleDescription from '../components/TitleDescription';
 
 function HomePage() {
 	return (
@@ -11,26 +12,11 @@ function HomePage() {
 					container
 					sx={{ marginTop: '5rem', textTransform: 'none' }}
 				>
-					<Grid sx={{ width: '100%' }}>
-						<Typography
-							variant='h2'
-							component={'h1'}
-							fontWeight={500}
-							mb={10}
-						>
-							Välkommen Till Ditt Jönköping
-						</Typography>
-						<Typography
-							variant='h4'
-							component={'h4'}
-							fontWeight={400}
-						>
-							Jönköping City är fyllt av butiker, kulter, upplevelser, caféer
-							och <br />
-							restauranger som ramas in av fantastiska grönområden och vackra
-							sjöar.
-						</Typography>
-					</Grid>
+					<TitleDescription
+						title='Välkommen Till Ditt Jönköping'
+						description='Jönköping City är fyllt av butiker, kulter, upplevelser, caféer
+							och restauranger som ramas in av fantastiska grönområden och vackra sjöar.'
+					/>
 				</Grid>
 			</Container>
 			<Box
