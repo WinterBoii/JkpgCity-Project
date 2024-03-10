ed0666cda3572162a884a09cfbf77e8f826e34cd
Akida27 <Shukmah01a@gmail.com>
2024-03-10 18:19:12 +0100

    created insertData file for the json files

--- backend/src/dbConnection.js
+++ backend/src/dbConnection.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
+const insertData = require('./inserData');
 require('dotenv').config;
 // Import your data insertion functions
-const { insertWellnessFromWellnessData } = require('./models/Wellness');
-const { insertStoresFromStoresData } = require('./models/Store');
+
 
 const dbURI = `mongodb+srv://wajd:admin2024@jkpcity.sktnmlb.mongodb.net/db`;
 const dbConnection = mongoose
 	.connect(dbURI)
 	.then(async () => {
 		console.log('Connected to cloud Atlas');
-		/* await insertStoresFromStoresData();
-		await insertWellnessFromWellnessData(); */
+		// await insertData.insertStoresFromStoresData()
+		// insertData.insertWellnessFromWellnessData()
 	})
 	.catch((error) => console.log(`Error connecting to the database "${error})`));
 
