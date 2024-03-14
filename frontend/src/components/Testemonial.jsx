f5f16df15c12e677f33c48e0cc73d760e1cfa79c
WinterBoii <wintercodess@gmail.com>
2024-03-14 22:25:07 +0100

    fix icons

--- frontend/src/components/Testemonial.jsx
+++ frontend/src/components/Testemonial.jsx
@@ -1,45 +1,75 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import { Typography, IconButton, Avatar } from '@mui/material';
-import { ArrowBack, ArrowForward } from '@mui/icons-material';
+import { Typography, IconButton, Avatar, Box } from '@mui/material';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import profileImg from '../assets/background.jpg';
 
 export default function Testimonial() {
-  const [currentIndex, setCurrentIndex] = React.useState(0);
-  const testimonials = [
-    { id: 1, image: profileImg, text: "Great service! Highly recommended." },
-    { id: 2, image: "/path_to_your_image2.jpg", text: "Awesome experience! Will definitely come back." },
-    
-  ];
+	const [currentIndex, setCurrentIndex] = React.useState(0);
+	const testimonials = [
+		{ id: 1, image: profileImg, text: 'Great service! Highly recommended.' },
+		{
+			id: 2,
+			image: '/path_to_your_image2.jpg',
+			text: 'Awesome experience! Will definitely come back.',
+		},
+	];
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
-  };
+	const handleNext = () => {
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+	};
 
-  const handlePrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length);
-  };
+	const handlePrevious = () => {
+		setCurrentIndex(
+			(prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length
+		);
+	};
 
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '90px', paddingBottom: '100px' }}>
-      <Typography variant="h2" gutterBottom style={{ fontWeight: 400 }}>
-        Testimonial
-      </Typography>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <div style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
-        
-          <IconButton aria-label="previous" onClick={handlePrevious} style={{ marginRight: '60px' }}>
-            {'<'} 
-          </IconButton>
-          <Avatar alt="Testimonial Image" src={testimonials[currentIndex].image} sx={{ width: 100, height: 100 }} />
-          <IconButton aria-label="next" onClick={handleNext} style={{ marginLeft: '60px' }}>
-            {'>'} 
-          </IconButton>
-        </div>
-        <Typography variant="body1" gutterBottom style={{ marginTop: '50px' }}>
-          {testimonials[currentIndex].text}
-        </Typography>
-      </div>
-    </div>
-  );
+	return (
+		<Box
+			sx={{ textAlign: 'center', paddingTop: '90px', paddingBottom: '100px' }}
+		>
+			<Typography
+				variant='h2'
+				gutterBottom
+				sx={{ fontWeight: 400 }}
+			>
+				Testimonial
+			</Typography>
+			<Box
+				sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+			>
+				<Box
+					sx={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}
+				>
+					<IconButton
+						aria-label='previous'
+						onClick={handlePrevious}
+						sx={{ marginRight: '60px' }}
+					>
+						<ArrowBackIosNewIcon />
+					</IconButton>
+					<Avatar
+						alt='Testimonial Image'
+						src={testimonials[currentIndex].image}
+						sx={{ width: 100, height: 100 }}
+					/>
+					<IconButton
+						aria-label='next'
+						onClick={handleNext}
+						sx={{ marginLeft: '60px' }}
+					>
+						<ArrowForwardIosIcon />
+					</IconButton>
+				</Box>
+				<Typography
+					variant='body1'
+					gutterBottom
+					sx={{ marginTop: '50px' }}
+				>
+					{testimonials[currentIndex].text}
+				</Typography>
+			</Box>
+		</Box>
+	);
 }
